2884605c34b8c3c9d0d608007db10d936798f2b0
Jonathan Pool <jonathan.pool@cvshealth.com>
2020-09-14 11:38:34 -0400

    Installed rally dependency

--- indextestrally.js
+++ indextestrally.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
-const rally = require('rally');
 const path = require('path');
 const axios = require('axios').default;
 const protocol = 'https';
 const urlBase = 'rally1.rallydev.com/slm/webservice/v2.0';
 const urlObj = 'user';
 const urlID = '123';
-const key = process.env.key;
+const key = process.env.RALLY_API_KEY;
 const url = `${protocol}://${path.join(urlBase, urlObj, urlID)}?key=${key}`;
 console.log(`About to query ${url}`);
 axios.get(url)
