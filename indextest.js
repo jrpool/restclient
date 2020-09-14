b96eb96d9fcb87c0e8fe615060f78a302410aaf1
Jonathan Pool <jonathan.pool@cvshealth.com>
2020-09-14 10:42:26 -0400

    Finished debugging first experiment

--- indextest.js
+++ indextest.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const axios = require('axios').default;
 const protocol = 'https';
-const urlBase = 'rally1.rallydev.com/slm/webservice/v2.0';
-const urlObj = 'user';
-const urlID = '123';
-const url = `${protocol}://${path.join(urlBase, urlObj, urlID)}`;
+const urlBase = 'api.covid19api.com';
+const urlEtc = 'world/total';
+const url = `${protocol}://${path.join(urlBase, urlEtc)}`;
 axios.get(url)
 .then(
     response => {
