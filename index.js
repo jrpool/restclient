3b35af3c4bb6c84588ef1a6cdbd1820aea1a2f85
Jonathan Pool <jonathan.pool@cvshealth.com>
2020-09-14 10:07:29 -0400

    Debugged 1st experiment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const axios = require('axios').default;
-const urlBase = 'https://covid19api.com';
+const urlBase = 'https://api.covid19api.com';
 const urlEtc = 'world/total';
 axios.get(`${urlBase}/${urlEtc}`)
-.then(console.log(response));
+.then(response => {
+    console.log(response.data);
+});
