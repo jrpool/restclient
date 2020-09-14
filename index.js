3169fed5cb89107d34916ed9545d7101ab175778
Jonathan Pool <jonathan.pool@cvshealth.com>
2020-09-14 11:02:03 -0400

    Started first practical experiment

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const path = require('path');
 const axios = require('axios').default;
 const protocol = 'https';
 const urlBase = 'rally1.rallydev.com/slm/webservice/v2.0';
 const urlObj = 'user';
 const urlID = '123';
-const url = `${protocol}://${path.join(urlBase, urlObj, urlID)}`;
+const key = process.env.key;
+const url = `${protocol}://${path.join(urlBase, urlObj, urlID)}?key=${key}`;
+console.log(`About to query ${url}`);
 axios.get(url)
 .then(
     response => {
